b6062efeb429952fd47c0d12871e19eef9052740
Jaideep Sundaram <jsundaram@pgdx.com>
2019-05-26 15:57:55 -0400

    Adds support for aliases and shell scripts

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+alias jira-add-comment="~/jira-python-utils/jira_add_comment.sh "
+alias jira-assign-issue="~/jira-python-utils/jira_assign_issue.sh "
+alias jira-get-issue-details="~/jira-python-utils/jira_get_issue_details.sh "
+alias jira-remove-watcher="~/jira-python-utils/jira_remove_watcher.sh "
